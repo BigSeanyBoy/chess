3f11a1e42e3e4408e07ad96394636783faf679bc
Sean Hammell <sean.m.hammell@gmail.com>
2022-10-22 14:10:01 -0500

    Remove redundant null character from fen string

--- main.c
+++ main.c
@@ -85,7 +85,7 @@ int main()
         printf("\033[0;0H\033[2J");
         printf("Welcome to the \x1b[34;1mNeptune Chess Engine\x1b[0m!\n");
         struct CBoard position;
-        char *fenstr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1\0";
+        char *fenstr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         import_fenstr(&position, fenstr);
 
         // char engine;
