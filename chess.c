1684652a18bc460737a8ce5511d25ab54aee5431
Sean Hammell <sean.m.hammell@gmail.com>
2022-11-11 15:34:28 -0600

    Begin perft

--- chess.c
+++ chess.c
@@ -5,16 +5,35 @@
 
 #include <stdio.h>
 
+U64 perft(struct position *state, int depth) {
+	U16 movelist[256];
+
+	if (depth == 0 && !incheck(state)) {
+		return 1ull;
+	}
+
+	int count = gendriver(state, movelist);
+	struct position statecopy;
+	U64 nodes = 0;
+	for (int i = 0; i < count; ++i) {
+		copy(state, &statecopy);
+		make(movelist[i], &statecopy);
+		U64 enemybb = statecopy.boards[flip(statecopy.side)];
+		if ((statecopy.boards[KING] & enemybb) == 0) {
+			return 0ull;
+		}
+		nodes += perft(&statecopy, depth - 1);
+	}
+	
+	return nodes;
+}
+
 int main(int argc, char *argv[]) {
 	struct position state;
 	char *fenstr = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 	setpos(&state, fenstr);
 
-	U16 movelist[256] = {0};
-	printf("%d\n", gendriver(&state, movelist));
-
-	struct position cstate;
-	copy(&state, &cstate);
+	printf("%llu\n", perft(&state, 3));
 
 	free(state.rays);
 
